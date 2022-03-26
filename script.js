4678d29571dcf860a1736f3dc30c277a89d24fb6
suchita-shakya <suchita.shakya2016@gmail.com>
2022-03-26 10:36:17 +0545

    Added code on shop, blog and product page

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const bar = document.getElementById('bar');
 const nav = document.getElementsByClassName('navbar');
+const close = document.getElementById('close');
 
-
+if(bar){
     bar.addEventListener('click', () => {
-        nav.classList.toggle('active');
+        nav.classList.add('show');
     });
+}
+
+if(close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('show');
+    });
+}
